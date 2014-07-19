9f90d1f8c778ddb4d6c45a60434ab7c9c756335f
Sharada <narayan_sharada@berkeley.edu>
2014-07-18 22:27:02 -0700

    integration

--- text_game.py
+++ text_game.py
@@ -141,9 +141,8 @@ def player(who):
 	else: #if who == 1
 		current_player, opponent_player = player2, player1
 
-
-
-def take_turn(current_player, opponent_player):
+def take_turn(player, opponent):
+	player.attack(player, opponent)
 	
 		
 
@@ -156,3 +155,70 @@ def take_turn(current_player, opponent_player):
 
 
 play_game()
+
+######################
+
+class Player:
+	life = 500
+	damage = 75
+	def __init__(self, name, x_coord, y_coord):
+		self.name = name
+		self.x_coord = x_coord
+		self.y_coord = y_coord
+
+	def reduce_health(self, hit_points):
+		if self.life <= hit_points:
+			print ("You're dead!!!")
+		else:
+			self.life -= hit_points
+
+	def attack(self, opponent, damage):
+		print (self.name, " has attacked ", opponent, " with ", damage, " damage !")
+		opponent.reduce_health(damage)
+		print(opponent.name, " has ", opponent.life, " life.")
+		
+
+class Instructor(Player):
+	life = 1000
+	damage = range(60,100)
+	
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def attack(self, opponent):
+		turn_damage = random_choice(self.damage)
+		Player.attack(self, opponent, turn_damage)
+		if turn_damage in range(90, 100):
+			print("SURPRISE EXAM! SUPER EFFECTIVE!!!")
+		elif turn_damage in range(60,70):
+			print("Nobody shows up for lecture. Not so effective.")
+		else:
+			print("INSTRUCTOR ATTACK!")
+		
+		
+class TA(Player):
+	life = 750
+
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def attack(self, opponent):
+		Player.attack(self, opponent, damage)
+		print("GUERILLA ATTACK!")
+		
+		
+class Reader(Player):
+	damage = range(50,90)
+
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def attack(self, opponent):
+		turn_damage = random_choice(self.damage)
+		Player.attack(self, opponent, turn_damage)
+		if turn_damage in range(50,60):
+			print("Oh no! This midterm is really long!")
+		elif turn_damage in range(80,90):
+			print("SUPER READER READS ALL THE EXAMS!")
+		else:
+			print('Reader attack!')
