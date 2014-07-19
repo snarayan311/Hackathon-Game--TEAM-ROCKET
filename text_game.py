37ecdab1b4e1925d180bc210d5882e4885871c2d
Sharada <narayan_sharada@berkeley.edu>
2014-07-18 21:16:52 -0700

    STARTING THE TEXT GAME FO REAL NOW

---
+++ text_game.py
@@ -0,0 +1,20 @@
+def other(who):
+	return 1 - who
+
+def play_game():
+	done = False
+	#initialize players
+	print ("WELCOME TO BERKELEY POKEMON BATTLE!")
+	player1_class = str(input("Player 1, choose your Pokemon: Instructor, Teaching Assistant, or Reader?"))
+	if player1_class == "Instructor:"
+		player1_name = str(input("Choose your instructor: Rohin or Andrew"))
+		if player1_name == 'Rohin':
+			#create Rohin object
+		else:
+			#create Andrew object
+
+
+	while not done:
+		#game-play
+
+play_game()
