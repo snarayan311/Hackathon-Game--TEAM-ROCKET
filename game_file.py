f0b302fad84ad9368278aef1e78441bff6210ec5
Andrew Blum <andrewblum@berkeley.edu>
2014-07-18 19:15:33 -0700

    fuckthis

--- game_file.py
+++ game_file.py
@@ -6,15 +6,15 @@ size = (700, 500)
 screen = pygame.dislay.set_mode(size)
 pygame.display.set_caption("cs61a pokemon")
 
-done = Falase
+done = False
 
 clock = pygame.time.Clock()
 
 #main program loop
 while not done:
 	#main event loop
-	for event in pygame.event.get()
-		if even.type == pygame.QUIT
+	for event in pygame.event.get():
+		if even.type == pygame.QUIT:
 			done = True
 
 	#game logic
