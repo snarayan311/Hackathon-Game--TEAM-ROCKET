ceb17180077afb2e2e0d888a20649c6569084b35
Andrew Blum <andrewblum@berkeley.edu>
2014-07-18 19:33:29 -0700

    i

--- game_file.py
+++ game_file.py
@@ -1,6 +1,12 @@
 
-import pygame 
+import pygame
 pygame.init()
+
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+BLUE = ( 0, 0, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
 
 size = (700, 500)
 screen = pygame.dislay.set_mode(size)
@@ -14,11 +20,27 @@ clock = pygame.time.Clock()
 while not done:
 	#main event loop
 	for event in pygame.event.get():
-		if even.type == pygame.QUIT:
+		if event.type == pygame.QUIT:
 			done = True
 
 	#game logic
 
 	#drawing code
+	screen.fill(WHITE)
 
+	pygame.draw.line(screen, GREEN, [0,0], [100, 100], 5)
+
+	pygame.draw.rect(screen, BLACK, [20, 20, 250, 100], 2)
+
+	font = pygame.font.SysFont('Calibri', 25, True, False)
+	text = font.render("My text", True, BLACK)
+	screen.blit(text, [250, 250])
+
+	clock.tick(60)
+
+
+
+	pygame.display.flip()
+
+pygame.quit()
 	
