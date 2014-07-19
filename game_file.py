96ced400c66c2f2a3659e7bf110771521799d571
Sharada <narayan_sharada@berkeley.edu>
2014-07-18 19:35:44 -0700

    starting with player classes

--- game_file.py
+++ game_file.py
@@ -14,11 +14,40 @@ clock = pygame.time.Clock()
 while not done:
 	#main event loop
 	for event in pygame.event.get():
-		if even.type == pygame.QUIT:
+		if event.type == pygame.QUIT:
 			done = True
 
 	#game logic
 
 	#drawing code
 
+class Player:
+	life = 500
+	damage = 75
+	def __init__(self, name):
+		self.name = name
+
+class Instructor(Player)
+	life = 1000
+	damage = 100 
 	
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def instructor_attack():
+
+class TA(Player)
+	life = 750
+
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def ta_attack():
+
+class Reader(Player)
+	damage = 50
+
+	def __init__(self, name):
+		Player.__init__(self, name)
+
+	def reader_attack():
