d3bdc9fa38fd7049f9005d1031627b411a7dea7a
Andrew Blum <andrewblum@berkeley.edu>
2014-07-18 18:36:09 -0700

    first

--- game_file.py
+++ game_file.py
@@ -1,10 +1,24 @@
-print('Team Rocket Go!')
-print(‘changed’)
 
 import pygame 
 pygame.init()
 
 size = (700, 500)
 screen = pygame.dislay.set_mode(size)
+pygame.display.set_caption("cs61a pokemon")
 
-print('Louis')
+done = Falase
+
+clock = pygame.time.Clock()
+
+#main program loop
+while not done:
+	#main event loop
+	for event in pygame.event.get()
+		if even.type == pygame.QUIT
+			done = True
+
+	#game logic
+
+	#drawing code
+
+	
